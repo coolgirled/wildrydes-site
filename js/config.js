59fbaeccb1f806b788bc826839388f8b993ea647
coolgirled <161401010+coolgirled@users.noreply.github.com>
2024-12-24 14:15:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_5S7HgbzKx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4pajg0651smo3fkrk68johf2ps', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
